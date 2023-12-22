e2473fa6923951dda3ba4e1f4505b10742d3363b
huw <huw.price@excelitas.com>
2023-12-22 07:18:11 +0000

    temperature dependent RGB, setpoint ready

--- main.cpp
+++ main.cpp
@@ -39,10 +39,32 @@ static uint8_t rVal = 0;
 static Point text_location(0, 0);
 
 static float setPoint{50};
-static float currentTemp{0};
+static float currentTemperature{0};
+
+typedef struct
+{
+  int red;
+  int green;
+  int blue;
+} RGB_t;
+
+static bool ready = true;
 
 int main()
 {
+  // Define RGB values for a gradient from blue to green/yellow to red
+  RGB_t gradient[] = {
+      {0, 0, 255},    // Blue
+      {30, 144, 255}, // Dodger Blue
+      {0, 255, 0},    // Lime Green
+      {255, 255, 0},  // Yellow
+      {255, 165, 0},  // Orange
+      {255, 69, 0},   // Red-Orange
+      {255, 0, 0},    // Red
+      {128, 0, 0},    // Maroon
+      {139, 0, 139},  // Dark Magenta
+      {148, 0, 211}   // Dark Violet
+  };
 
   stdio_init_all();
   One_wire one_wire(2); // GP2
@@ -52,7 +74,6 @@ int main()
 
   st7789.set_backlight(255);
 
-  Pen BG = graphics.create_pen(200, 0, 0);
   Pen WHITE = graphics.create_pen(255, 255, 255);
 
   printf("\nBegin main loop");
@@ -60,6 +81,30 @@ int main()
 
   while (true)
   {
+    one_wire.single_device_read_rom(address);
+    one_wire.convert_temperature(address, false, false);
+    currentTemperature = one_wire.temperature(address);
+    // Calculate the index in the gradient array based on the temperature
+    int index = ((int)round(currentTemperature) * 9) / 100;
+    // Create a pen with the RGB values from the selected index
+    Pen BG = graphics.create_pen(gradient[index].red, gradient[index].green, gradient[index].blue);
+    // Set the background colour
+    graphics.set_pen(BG);
+    graphics.clear();
+
+    // Check if the currentTemperature is within 2 degrees of setPoint
+    int withinTwoDegrees = (currentTemperature >= setPoint - 2 && currentTemperature <= setPoint + 2);
+
+    ready = withinTwoDegrees ? true : false;
+
+    if (ready)
+    {
+      led.set_rgb(gradient[index].red, gradient[index].green, gradient[index].blue);
+    }
+    else
+    {
+      led.set_rgb(0, 0, 0);
+    }
 
     if (button_a.raw())
     {
@@ -89,12 +134,9 @@ int main()
     {
       if (setPoint <= 100.0)
       {
-        setPoint += 5.0;
+        setPoint += 1.0;
       }
     }
-
-    graphics.set_pen(BG);
-    graphics.clear();
 
     graphics.set_pen(WHITE);
 
@@ -103,39 +145,27 @@ int main()
     graphics.text("--- Wax Melt Monitor ---", text_location, 320, 2);
     text_location.x -= 48;
 
-    one_wire.single_device_read_rom(address);
-    one_wire.convert_temperature(address, true, false);
-    currentTemp = one_wire.temperature(address);
-
     graphics.set_font(&font14_outline);
     text_location.y += rowHeight * 5;
-    text_location.x += 135;
-    snprintf(printBuf, 256, "%3.1f C", currentTemp);
+    text_location.x += 130;
+    snprintf(printBuf, 256, "%3.1f C", currentTemperature);
     graphics.text(printBuf, text_location, 320);
-    text_location.x -= 135;
-    text_location.y -= rowHeight * 5;
+    text_location.x -= 130;
 
-    toggle_rVal();
-    led.set_rgb(0, rVal, 0);
-    sleep_ms(10);
+    text_location.y += rowHeight * 5;
+    text_location.x += 20;
+    graphics.set_font(&font8);
+    snprintf(printBuf, 256, "Set point: %3.1f C", setPoint);
+    graphics.text(printBuf, text_location, 320);
+    text_location.x -= 20;
+
+    text_location.y -= rowHeight * 10;
+
+    sleep_ms(50);
 
     // update screen
     st7789.update(&graphics);
   }
 
   return 0;
-}
-
-void toggle_rVal(void)
-{
-  if (toggle_led)
-  {
-    rVal = 255;
-    toggle_led = false;
-  }
-  else
-  {
-    rVal = 0;
-    toggle_led = true;
-  }
 }
